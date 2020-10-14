4f6d126e616390bde6aaf12dcbc623a4ad20e9a3
Rafael Makaha <rafael.makaha@gmail.com>
2020-10-14 18:58:46 -0300

    Add socket.io to html script
    Co-authored-by: Wesley Adriann <wesleyadriann@gmail.com>

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,11 @@
 import { A } from './app.js';
+const { io } = window;
 
-console.log(A);
+const socket = io();
+
+const text = document.getElementById("text");
+
+socket.on('emitX', (dados) => {
+    console.log(dados);
+    text.innerHTML = dados;
+});
