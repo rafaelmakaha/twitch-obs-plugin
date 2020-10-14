4ff637ed48d333b012e1e6382bfb5619efc8e3cc
Rafael Makaha <rafael.makaha@gmail.com>
2020-10-14 18:56:25 -0300

    Fix socket.io on index.js
    Co-authored-by: Wesley Adriann <wesleyadriann@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ const CreateBot = require('./model/CreateBot');
 const express = require('express');
 const app = require('express')();
 const httpServer = require('http').createServer(app);
-var io = require('socket.io')(httpServer);
+var io = require('socket.io')(httpServer,{path: '/socket.io'});
 
-io.on("connection", (socket) => {
-	const bot = CreateBot(socket.emit);
-});
-
+const bot = new CreateBot(io.emit.bind(io));
 app.use(express.static(__dirname + '/js'));
+
+io.on('connection', (socket) => {
+	console.log("User connected!")
+});
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
