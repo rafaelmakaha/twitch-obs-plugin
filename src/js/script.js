ec0230f48bfd8c6551686de390645778531c7c2e
Rafael Makaha <rafael.makaha@gmail.com>
2020-10-15 14:29:07 -0300

    Add timer between messages
    Co-authored-by: Wesley Adriann <wesleyadriann@gmail.com>

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,17 @@
 import { A } from './app.js';
 const { io } = window;
 
-const socket = io('http://localhost:3000');
+const socket = io(window.location.href);
 
 const text = document.getElementById("text");
 
 socket.on('emitX', (dados) => {
     console.log(dados);
     text.innerHTML = `${dados.author}: ${dados.message}`;
+    setTimeout(() => {
+        text.innerHTML = '';
+    }, 5000);
+    setTimeout(() => {
+        socket.emit('freeFront', true);
+    }, 6000);
 });
