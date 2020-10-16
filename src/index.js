50160987cb86359b552797fbb66a903f690dd885
Rafael Makaha <rafael.makaha@gmail.com>
2020-10-16 15:24:22 -0300

    Add cors to application
    Co-authored-by: Wesley Adriann <wesleyadriann@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ const express = require('express');
 const app = express();
 const httpServer = require('http').createServer(app);
 const io = require('socket.io')(httpServer,{path: '/socket.io'});
+const cors = require('cors');
 
 
+app.use(cors());
 const queue = new AlertQueue(io.emit.bind(io));
-
 const bot = new CreateBot(queue.queue);
 
 app.use(express.static(__dirname + '/js'));
@@ -16,7 +17,6 @@ io.on('connection', (socket) => {
 	console.log("User connected!")
 	socket.on('freeFront', queue.free);
 });
-
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
