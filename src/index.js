7196bb177b70229b92baf82f64a45810ba600998
Rafael Makaha <rafael.makaha@gmail.com>
2020-11-03 18:19:44 -0300

    Rework folder tree
    Co-authored-by: Wesley Adriann <wesleyadriann@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ const app = express();
 const httpServer = require('http').createServer(app);
 const io = require('socket.io')(httpServer,{path: '/socket.io'});
 const cors = require('cors');
+const path = require('path');
 
 
 app.use(cors());
 const queue = new AlertQueue(io.emit.bind(io));
 const bot = new CreateBot(queue.queue);
 
-app.use(express.static(__dirname + '/js'));
+app.use(express.static(path.join(__dirname, '/static')));
 
 io.on('connection', (socket) => {
 	console.log("User connected!")
@@ -19,7 +20,7 @@ io.on('connection', (socket) => {
 });
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(path.join(__dirname, '/static/index.html'));
 });
 
 httpServer.listen(3000, () => {
