3ed63d6eab8d7ba37f4ae5177e6962a3a8dc3435
Rafael Makaha <rafael.makaha@gmail.com>
2020-12-27 14:25:11 -0300

    Fix < and > tags on message.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- src/model/tts.js
+++ src/model/tts.js
@@ -1,15 +1,24 @@
 const axios = require('axios');
 
+function filterTags(msg) {
+  // console.log('msg: ', msg);
+  // console.log('msg type: ', typeof msg);
+  const newMsg = msg.replace(/</g, ' menor que ')
+  return newMsg.replace(/>/g, ' maior que ')
+}
+
 function tts(msg, voice) {
   const url = 'https://ttsmp3.com/makemp3_new.php';
   const source = 'ttsmp3';
+  const filteredMsg = filterTags(msg);
+  console.log(filteredMsg);
 
   return new Promise((resolve, reject) => {
     axios({
       method:'post',
       url: url,
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      data: `msg=${msg}&lang=${voice}&source=${source}`
+      data: `msg=${filteredMsg}&lang=${voice}&source=${source}`
     })
     .then(resolve)
     .catch(reject)
