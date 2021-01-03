193b4e6963e0282830ad7bffb65ec881d4c1c5ef
Wesley Adriann <wesleyadriann@gmail.com>
2021-01-02 16:07:24 -0800

    Adds to connect only one client in socket
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,28 +1,38 @@
-const CreateBot = require('./model/CreateBot');
-const AlertQueue = require('./model/AlertQueue')
 const express = require('express');
-const app = express();
-const httpServer = require('http').createServer(app);
-const io = require('socket.io')(httpServer,{path: '/socket.io'});
-const cors = require('cors');
+const http = require('http');
+const socketIo = require('socket.io')
 const path = require('path');
 
+const MessagesQueue = require('./model/MessagesQueue');
+const CreateBot = require('./model/CreateBot');
 
-app.use(cors());
-const queue = new AlertQueue(io.emit.bind(io));
-const bot = new CreateBot(queue.queue);
+const app = express();
+
+const httpServer = http.createServer(app)
+const io = socketIo(httpServer, {path: '/socket.io'});
+
+const queue = new MessagesQueue(io.emit.bind(io));
+new CreateBot(queue.queue);
 
 app.use(express.static(path.join(__dirname, '/static')));
 
+const connectedSocketClients = []
+
 io.on('connection', (socket) => {
-	console.log("User connected!")
+	console.log('User connected!');
+	const hasClients = !!connectedSocketClients.length;
+	console.log('Has Clients:', hasClients);
+	if(hasClients) return socket.disconnect(true);
+	
+	connectedSocketClients.push(socket)
 	socket.on('freeFront', queue.free);
+	socket.on('disconnect', () => connectedSocketClients.pop())
 });
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '/static/index.html'));
-});
+
+// app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/static/index.html')));
+
 
 httpServer.listen(3000, () => {
-	console.log('listening on *:3000');
+	console.log('Server listenin on :3000');
 });
