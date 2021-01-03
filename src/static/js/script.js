ef60d5c4d02f0fd9790b1a8c40767685f011b756
Wesley Adriann <wesleyadriann@gmail.com>
2021-01-02 16:06:34 -0800

    Removes try catch block
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -4,36 +4,32 @@ const { io } = window;
 const socket = io(window.location.href);
 const container = document.getElementById('container');
 
-socket.on('emitX', async (dados) => {
-    try {
-        console.log(dados);
-        let time = 3000;
-        
-        if (dados.image){
-            const image = await imageLoad(dados.image);
-            container.appendChild(image);
-        }
+socket.on('showMessage', (dados) => {
+    console.log(dados);
+    let time = 3000;
 
-        if (dados.sound){
-            const audio = await audioLoad(dados.sound.basePath, dados.sound.mp3)
-            container.appendChild(audio);
-            if (audio.duration > time/1000)
-                time = (audio.duration + 1) * 1000
-        }
-
-        const h2 = document.createElement('h2');
-        h2.innerHTML = `${dados.author}: \n${dados.message}`;
-        container.appendChild(h2);
-
-        setTimeout(() => {
-            while (container.firstChild) {
-                container.removeChild(container.lastChild);
-            }
-        }, time);
-        setTimeout(() => {
-            socket.emit('freeFront', true);
-        }, time + 1000);
-    } catch (error) {
-        console.log('Error on generating frontEnd',error)
+    if (dados.image){
+        const image = await imageLoad(dados.image);
+        container.appendChild(image);
     }
+
+    if (dados.sound){
+        const audio = await audioLoad(dados.sound.basePath, dados.sound.mp3);
+        container.appendChild(audio);
+        if (audio.duration > time/1000)
+            time = (audio.duration + 1) * 1000
+    }
+
+    const h2 = document.createElement('h2');
+    h2.innerHTML = `${dados.author}: \n${dados.message}`;
+    container.appendChild(h2);
+
+    setTimeout(() => {
+        while (container.firstChild) {
+            container.removeChild(container.lastChild);
+        }
+
+        setTimeout(() => socket.emit('freeFront', true), 1000);
+    }, time);
+
 });
