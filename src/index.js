2930d79da7def1bb895da8fa75bc51ec4351eb71
Wesley Adriann <wesleyadriann@gmail.com>
2021-01-02 17:09:58 -0800

    Adds try catch block
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,27 +6,39 @@ const path = require('path');
 const MessagesQueue = require('./model/MessagesQueue');
 const CreateBot = require('./model/CreateBot');
 
-const app = express();
-const httpServer = http.createServer(app);
+const startServer = async () => {
+	try {
+		const app = express();
+		const httpServer = http.createServer(app);
 
-const io = socketIo(httpServer, {path: '/socket.io'});
+		const io = socketIo(httpServer, {path: '/socket.io'});
 
-const messagesQueue = new MessagesQueue(io.emit.bind(io));
-new CreateBot(messagesQueue.queue);
+		const messagesQueue = new MessagesQueue(io.emit.bind(io));
+		new CreateBot(messagesQueue.queue);
 
-app.use(express.static(path.join(__dirname, '/static')));
+		app.use(express.static(path.join(__dirname, '/static')));
 
-const connectedSocketClients = [];
+		const connectedSocketClients = [];
 
-io.on('connection', (socket) => {
-	console.log('User connected!');
-	const hasClients = !!connectedSocketClients.length;
-	console.log('Has Clients:', hasClients);
-	if(hasClients) return socket.disconnect(true);
-	
-	connectedSocketClients.push(socket)
-	socket.on('freeFront', messagesQueue.free);
-	socket.on('disconnect', () => connectedSocketClients.pop())
-});
+		io.on('connection', (socket) => {
+			console.log('User connected!');
+			const hasClients = !!connectedSocketClients.length;
+			console.log('Has Clients:', hasClients);
+			if(hasClients) return socket.disconnect(true);
+			
+			connectedSocketClients.push(socket)
+			socket.on('freeFront', messagesQueue.free);
+			socket.on('disconnect', (reason) => {
+				console.log('socket disconnected: ', reason);
+				connectedSocketClients.pop()
+			})
+		});
 
-httpServer.listen(3000, () => console.log('Server listenin on :3000'));
+		httpServer.listen(3000, () => console.log('Server listenin on :3000'));
+
+	} catch (error) {
+		console.log('Error in start server', error)
+	}
+}
+
+startServer();
--- src/static/js/index.js
+++ src/static/js/index.js
@@ -5,29 +5,33 @@ const socket = io(window.location.href);
 const container = document.getElementById('container');
 
 socket.on('showMessage', async (dados) => {
-    console.log('recived message',dados);
-    let time = 3000;
-    let image = null, sound = null;
-
-    if (dados.image) image = await imageLoad(dados.image);
-        
-    if (dados.sound){
-        audio = await audioLoad(dados.sound.basePath, dados.sound.mp3);
-        if (audio.duration > time/1000) time = (audio.duration + 1) * 1000
-    }
-
-    const h2 = document.createElement('h2');
-    h2.innerHTML = `${dados.author}: \n${dados.message}`;
-
-    if(image) container.appendChild(image);
-    if(sound) container.appendChild(audio);
-    container.appendChild(h2);
-
-    setTimeout(() => {
-        while (container.firstChild) {
-            container.removeChild(container.lastChild);
+    try {
+        console.log('recived message',dados);
+        let time = 3000;
+        let image = null, sound = null;
+    
+        if (dados.image) image = await imageLoad(dados.image);
+            
+        if (dados.sound){
+            audio = await audioLoad(dados.sound.basePath, dados.sound.mp3);
+            if (audio.duration > time/1000) time = (audio.duration + 1) * 1000
         }
-        setTimeout(() => socket.emit('freeFront', true), 1000);
-    }, time);
+    
+        const h2 = document.createElement('h2');
+        h2.innerHTML = `${dados.author}: \n${dados.message}`;
+    
+        if(image) container.appendChild(image);
+        if(sound) container.appendChild(audio);
+        container.appendChild(h2);
+    
+        setTimeout(() => {
+            while (container.firstChild) {
+                container.removeChild(container.lastChild);
+            }
+            setTimeout(() => socket.emit('freeFront', true), 1000);
+        }, time);
+    } catch (error) {
+        console.log('Error no showMessage: ', error)
+    }
 
 });
