0921dd93c4d8f1c230a2268b592f71c2958c046a
Rafael Makaha <rafael.makaha@gmail.com>
2021-01-27 15:29:45 -0300

    Update settings routes.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- src/routes/settings.js
+++ src/routes/settings.js
@@ -1,14 +1,19 @@
 const router = require('express').Router();
 const fs = require('fs');
+const path = require('path');
 
 // define the home page route
 router.get('/settings', (req, res) => {
-    const settings = require('../settings/settings.json');
+    const settings = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'settings', 'settings.json')));
     res.json(settings)
 });
 // define the about route
 router.put('/settings', (req, res) => {
-    res.send('About birds');
+    fs.writeFileSync(path.join(__dirname, '..', 'settings', 'settings.json'), 
+                        JSON.stringify(req.body))
+
+    req.bot.configureRewards();
+    res.json(req.body);
 });
 
 module.exports = router;
