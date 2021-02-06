493a26282900d3f45d352b6d19e145b95c1e73b0
Rafael Makaha <rafael.makaha@gmail.com>
2021-02-06 14:32:45 -0300

    Add validateToken method to settings.js
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- src/static/js/settings.js
+++ src/static/js/settings.js
@@ -5,13 +5,28 @@ const getToken = () => {
             const [accessToken] = location.hash.split('&')
             token = accessToken.split('=').pop()
             localStorage.setItem('tokenTwitch', token)
+        }else{
+            validateToken(token)
         }
         return token
     } catch (error) {
-        console.log('Error', error)
+        console.log('getToken Error: ', error)
     }
 }
 
-const validateToken = () => {
-    
+const validateToken = (token) => {
+    const url = 'https://id.twitch.tv/oauth2/validate'
+    fetch(url, {
+        headers: {
+            Authorization: 'OAuth ' + token
+        }
+    })
+    .then(response => response.json())
+    .then(response => {
+        if (response.ok)
+            console.log(response.json())
+    })
+    .catch(error => console.log('validateToken Error: ', error))
 }
+
+getToken()
