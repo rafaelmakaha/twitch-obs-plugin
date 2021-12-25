b37603678ef9923b01f20274a9eb7a23e0a5d427
Rafael Makaha <rafael.makaha@gmail.com>
2021-12-25 15:47:21 -0300

    feat: Add qrcode render and request
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 if (process.env.NODE_ENV !== 'production')
     require('dotenv').config()
 
+import express from 'express'
 import axios from 'axios'
-import fs, { access } from 'fs'
+import fs from 'fs'
 import path from 'path'
 import https from 'https'
 
@@ -19,19 +20,26 @@ const credentials = Buffer.from(
     `${process.env.GN_CLIENT_ID}:${process.env.GN_CLIENT_SECRET}`
 ).toString('base64');
 
-axios({
-    method: 'POST',
-    url: `${process.env.GN_ENDPOINT}/oauth/token`,
-    headers: {
-        Authorization: `Basic ${credentials}`,
-        'Content-Type': 'application/json'
-    },
-    httpsAgent: agent,
-    data: {
-        grant_type: 'client_credentials'
-    }
-}).then((response) => {
-    const accessToken = response.data?.access_token
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', 'src/views')
+
+app.get('/', async (req, res) => {
+
+    const authResponse = await axios({
+        method: 'POST',
+        url: `${process.env.GN_ENDPOINT}/oauth/token`,
+        headers: {
+            Authorization: `Basic ${credentials}`,
+            'Content-Type': 'application/json'
+        },
+        httpsAgent: agent,
+        data: {
+            grant_type: 'client_credentials'
+        }
+    })
+    const accessToken = authResponse.data?.access_token
 
     const reqGN = axios.create({
         baseURL: process.env.GN_ENDPOINT,
@@ -44,15 +52,22 @@ axios({
 
     const dataCob = {
         "calendario": {
-          "expiracao": 3600
+            "expiracao": 3600
         },
         "valor": {
-          "original": "100.00"
+            "original": "100.00"
         },
         "chave": "71cdf9ba-c695-4e3c-b010-abb521a3f1be",
         "solicitacaoPagador": "Informe o número ou identificador do pedido."
-      }
+    }
 
-    reqGN.post('v2/cob', dataCob).then(response => console.log(response.data))
-});
+    const cobResponse = await reqGN.post('v2/cob', dataCob)
+    const qrcodeResponse = await reqGN.get(`/v2/loc/${cobResponse.data.loc.id}/qrcode`)
 
+    res.render('qrcode', { qrcodeImage: qrcodeResponse.data.imagemQrcode})
+
+})
+
+app.listen(8000, () => {
+    console.log('running')
+})
